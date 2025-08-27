456306623788d42ee3c69489e022b7362c1b2c96
Mikael Regetz <127456294+MikaelRegetz10@users.noreply.github.com>
2025-08-27 10:38:32 -0300

    Updated index.jsx

--- src/pages/Register/index.jsx
+++ src/pages/Register/index.jsx
@@ -1,7 +1,6 @@
 import { FaArrowLeft } from "react-icons/fa"
 import { Link } from "react-router-dom"
 import { FormRegister } from "../../components/FormRegister"
-import styles from "./styles.module.scss"
 
 export const Register = () => {
      return (
