05a6937a06f9da8176bc19bda461d5ac1f3bfbfa
MikaelRegetz10 <mikaelregetz@gmail.com>
2025-08-27 17:01:39 -0300

    atualizacao de link api

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 // https://swim-tracker-api.onrender.com
 export const api = axios.create({
-    baseURL: "http://localhost:8080",
+    baseURL: "https://swim-tracker-api.onrender.com",
     timeout: 8 * 1000
 })
